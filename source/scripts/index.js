5cde6366ab7300a39af3cea29b38ad1d1de3a9da
Julia <130411614+Julyova@users.noreply.github.com>
2025-11-04 01:36:37 +0300

    стилизует декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--no-js');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    navToggle.classList.remove('main-nav__toggle--open');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    navToggle.classList.add('main-nav__toggle--open');
+  }
+});
